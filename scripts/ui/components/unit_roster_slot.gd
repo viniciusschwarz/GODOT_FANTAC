634404e3b7b34f4bf15bb8b64f376fa917cc3300
Vinicius Schwarz <vinicius.schwarz@gmail.com>
2026-08-07 12:09:17 -0300

    PROTO 3

--- scripts/ui/components/unit_roster_slot.gd
+++
@@ -1,4 +0,0 @@
-extends PanelContainer
-
-func setup(unit_data: Dictionary):
-	$HBoxContainer/NameLabel.text = unit_data.get("name", "Unknown Unit")
--- scripts/ui/screens/barracks_screen.gd
+++
@@ -1,7 +0,0 @@
-extends WindowBase
-
-func _ready() -> void:
-	# The DataManager and logic would populate this in a real scenario
-	# Example pseudo-code from design doc:
-	# SignalBus.emit_signal("request_player_roster_data")
-	pass
