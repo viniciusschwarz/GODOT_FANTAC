4a8c58e96c4ce3e1216f97f562c809d06449c97b
Vinicius Schwarz <vinicius.schwarz@gmail.com>
2026-09-19 22:43:34 -0300

    NEW

--- scripts/resources/turn_replay_buffer_resource.gd
+++
@@ -1,4 +0,0 @@
-class_name TurnReplayBufferResource extends Resource
-
-@export var turn_number: int = 0
-@export var tick_snapshots: Array[TickSnapshotData] = []
--- test_battlefield.gd
+++
@@ -1,6 +0,0 @@
-extends SceneTree
-
-func _init():
-    var matrix = load("res://scripts/core/battlefield_matrix.gd").new()
-    print("Matrix instantiated")
-    quit()
